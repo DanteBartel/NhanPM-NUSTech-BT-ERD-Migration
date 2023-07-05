902b1f5b18c74f3e462316184d4e2f32dc9b3cd4
NhanPM <dantedbartel@gmail.com>
2023-07-05 20:12:41 +0700

    230705 2012 Upload some view and using bootstrap

--- app/controllers/temps_controller.rb
+++ app/controllers/temps_controller.rb
@@ -1,4 +1,26 @@
 class TempsController < ApplicationController
   def signup
+
+  end
+  def login
+
+  end
+  def feed
+
+  end
+  def feed_album
+
+  end
+  def my_photo
+
+  end
+  def my_album
+
+  end
+  def new_photo
+
+  end
+  def edit_photo
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "temps#signup"
   get "/temps/signup", to: "temps#signup"
+  get "/temps/login", to: "temps#login"
+  get "/temps/feed", to: "temps#feed"
+  get "/temps/feed_album", to: "temps#feed_album"
+  get "/temps/my_photo", to: "temps#my_photo"
+  get "/temps/my_album", to: "temps#my_album"
+  get "/temps/new_photo", to: "temps#new_photo"
+  get "/temps/edit_photo", to: "temps#edit_photo"
 end
